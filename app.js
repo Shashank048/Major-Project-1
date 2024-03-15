7a40c02cb38465e01d4f371c3e54a71f138415b3
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-03-16 01:51:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+if(process.env.NODE_ENV != "production"){
+  require('dotenv').config();
+}
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate= require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const localstrategy = require("passport-local");
+const User = require("./models/user.js");
 
+
+const listingsRouter = require("./routes/listing.js");
+const reviewsRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -22,63 +36,69 @@ async function main() {
 }
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/view"));
+app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
-app.get("/", (req, res) => {
-  res.send("Hi, I am root");
+
+const sessionOption = {
+  secret : "mysupersecretcode",
+  resave:false,
+  saveUninitialized: true,
+  cookie: { 
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,   
+   }
+};
+
+//app.get("/", (req, res) => {
+//  res.send("Hi, I am root");
+//});
+
+app.use(session(sessionOption));
+app.use(flash());
+
+
+app.use(passpoert.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()) );
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req,res,next)=>{
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");  
+  res.kocals.currUser = req.user;
+  next();
 });
 
-//Index Route
-app.get("/listings", async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("listings/index.ejs", { allListings });
+//const validateReview = (req,res,next) => {
+//  let {error} = reviewSchema.validate(req.body);
+//  if(error) {
+//    let errMsg = error.details.map((el) => el.message).join(",");
+//    throw new ExpressError(400,errMsg);
+//  }else{
+//    next();
+//  }
+//}
+
+app.use("/listings",listingsRouter);
+app.use("/listings/:id/reviews", reviewsRouter );
+app.use("/",userRouter);
+
+app.all("*", (req,res,next) => {
+  next(new ExpressError(404,"PAGE NOT FOUND!"));
 });
 
-//New Route
-app.get("/listings/new", (req, res) => {
-  res.render("listings/new.ejs");
+app.use((err,req,res,next) =>{
+  let {statusCode = 500,message = "Something went Wrong!"} = err;
+  res.status(statusCode).render("error.ejs",{ message });
+// res.status(statusCode).send(message);
 });
-
-//Show Route
-app.get("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/show.ejs", { listing });
-});
-
-//Create Route
-app.post("/listings", async (req, res) => {
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
-});
-
-//Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", { listing });
-});
-
-//Update Route
-app.put("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  res.redirect(`/listings/${id}`);
-});
-
-//Delete Route
-app.delete("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  let deletedListing = await Listing.findByIdAndDelete(id);
-  console.log(deletedListing);
-  res.redirect("/listings");
-});
-
 
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
