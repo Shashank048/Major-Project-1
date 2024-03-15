2dc7a4c297cee441b9c994b52756a392c5bbc03e
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-03-16 02:11:03 +0530

    Create user.js

---
+++ routes/user.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const wrapAsync = require("../utils/wrapAsync");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware");
+
+const userController = require("../controllers/users.js");
+
+
+router.route("/signup")
+      .get(userController.renderSignupForm)
+      .post(wrapAsync(userController.signup));
+
+
+//router.get("/signup",userController.renderSignupForm);
+
+//router.post("/signup",wrapAsync(userController.signup));
+
+router.route("/login")
+        .get("/login",userController.renderLoginForm)
+        .post("/login",saveRedirectUrl, passport.authenticate("local",{
+            failureRedirect: "/login",
+            failureflash: true,    
+        }),
+        userController.login
+        );
+
+//router.get("/login",userController.renderLoginForm);
+
+
+//router.post("/login",saveRedirectUrl, passport.authenticate("local",{
+//    failureRedirect: "/login",
+//    failureflash: true,    
+//}),
+//userController.login
+//);
+
+
+router.get("/logout",userController.logout);
+
+
+module.exports = router();
