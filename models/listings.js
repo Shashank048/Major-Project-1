a316e2f3b3c01007f0c1814c23a5ce2a7ac9e4a4
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-03-16 02:04:15 +0530

    Update listings.js

--- models/listings.js
+++ models/listings.js
@@ -1,22 +1,46 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
+const { string } = require("joi");
 
 const listingSchema = new Schema({
     title: { 
         type : String,
-        required : true,
+        required : true,    
     },
     description: String,
     image: {
-        url: {
-            type: String,
-            default: "https://www.peakpx.com/en/hd-wallpaper-desktop-kqhta",
-
-    },
+        url: string,
+        filename: string,
 },
     price: Number,
     location: String,
     country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref:"Review"
+        },
+    ],
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    geometry: {
+        type: String,
+        enum: ['Point'],
+        required: true
+    },
+    coordinates: {
+        type: [Number],
+        required: true,
+    },
+});
+
+listingSchema.post("findOneAndDelete", async (listing)=>{
+    if(listing) {
+        await Review.deleteMany({reviews : {$in: listing.reviews}});
+    };
 });
 
 const Listing = mongoose.model("Listing",listingSchema);
