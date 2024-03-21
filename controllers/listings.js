8eba763d4a286602050513f54f7d8485317944a6
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-03-21 19:11:33 +0530

    Update listings.js

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,7 +1,7 @@
 const { REPL_MODE_SLOPPY } = require("repl");
 const Listing = require("../models/listing");
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const mapToken = process.env.MAP_ACCESS_TOKEN;
+const mapToken = process.env.MAP_TOKEN;
 const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 
