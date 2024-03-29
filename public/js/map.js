14916cc1a3463143fcc2e9dc26e50273353c3fac
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-03-29 14:06:03 +0530

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,15 +1,16 @@
- mapboxgl.accessToken = mapToken;
+
+mapboxgl.accessToken = mapToken;
  
-  mapboxgl.accessToken = mapToken;
-   const map = new mapboxgl.Map({
-    container: 'map',
-    style:'mapbox://styles/mapbox/dark-v12',
-    center: listing.geometry.coordinates,
-    zoom:9
-   });
+mapboxgl.accessToken = mapToken;
+ const map = new mapboxgl.Map({
+  container: 'map',
+  style:'mapbox://styles/mapbox/dark-v12',
+  center: listing.geometry.coordinates,
+  zoom:9
+ });
 
 const marker = new mapboxgl.Marker({Color: "red"})
-  .setLngLat(coordinates)
-  .setPopup(new mapboxgl.Popup({offset: 25})
-  ,setHTML(`<h4>${listing.location}</h4><p>Exact Location provided after booking</p>` ))
-  .addTo(map);
+.setLngLat(coordinates)
+.setPopup(new mapboxgl.Popup({offset: 25})
+,setHTML(`<h4>${listing.location}</h4><p>Exact Location provided after booking</p>` ))
+.addTo(map);
