70c3c86f0f0a7e5c5a8272aeda4d08d24185fbb7
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-04-13 18:08:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ const User = require("./models/user.js");
 
 
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+//const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+const dbUrl= process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -31,7 +33,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 
