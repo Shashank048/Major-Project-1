85c89202223006662c2c0b8258080896494f924a
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-04-19 13:23:46 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,15 +1,23 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema; 
-const passportLocalMongoose = require("passport-local-mongoose");
 
-const userSchema = new Schema({
-    email: {
-        type: String,
-        require:true,
-        unique: true,
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    comment: String,
+    rating: {
+        type:Number,
+        min:1,
+        max:5
     },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref:"User",
+    }
 });
 
-userSchema.plugin(passportLocalMongoose);
-
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("Review",reviewSchema);   
