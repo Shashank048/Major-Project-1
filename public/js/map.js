25cff5c9d32fe1987d894ec605af4a80ec3c3143
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-04-24 13:08:12 +0530

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -22,18 +22,17 @@ const map = new mapboxgl.Map({
   container: 'map', // container ID
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
   style: 'mapbox://styles/mapbox/streets-v12', // style URL
-  center: listing.geometry.coordinates, // starting position [lng, lat]
+  center:  [12.4822 , 41.8967], // starting position [lng, lat]  [41.8967, 12.4822]
   zoom: 9 // starting zoom
 });
 
-console.log(listing.geometry.coordinates);
 
 
-const marker = new mapboxgl.Marker({color:"red"})
-  .setLngLat(listing.geometry.coordinates)
-  .setPopup(
-    new mapboxgl.Popup({offset:25}).setHTML(
-      `<h5>${listing.title}</h5><p>Exact location will be provided after booking</p>`
-    )
-  )
-  .addTo(map);
+//const marker = new mapboxgl.Marker({color:"red"})
+//  .setLngLat(listing.geometry.coordinates)
+//  .setPopup(
+//    new mapboxgl.Popup({offset:25}).setHTML(
+//      `<h5>${listing.title}</h5><p>Exact location will be provided after booking</p>`
+//    )
+//  )
+//  .addTo(map);
