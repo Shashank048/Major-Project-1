9f3e447b967616af701139f1aa3d972e89848e7e
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2024-05-03 12:46:07 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -12,7 +12,7 @@ module.exports.listingSchema = Joi.object({
     }).required(),
 })
 
-module.exports.reviewSchema = Joi.object({
+module.exports.reviewSchema = Joi.object({      
     review : Joi.object({
         rating: Joi.number().required().min(0).max(5),
         comment: Joi.string().required(),
