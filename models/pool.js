fe1ecbf375ca1377468f2ee336f7861c6e23c496
Shashank048 <136824394+Shashank048@users.noreply.github.com>
2025-08-07 01:56:14 +0530

    Create pool.js

---
+++ models/pool.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const poolSchema = new Schema({
+    title: { type: String, required: true },
+    description: String,
+    image: {
+        url: String,
+        filename: String,
+    },
+    price: Number,
+    location: String,
+    country: String,
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+        },
+    },
+});
+
+module.exports = mongoose.model("Pool", poolSchema);
