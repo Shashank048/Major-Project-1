f0c2410de976c56437162834de0b54202a2b3d00
Shashank <choureshashank21@gmail.com>
2026-01-20 15:44:37 +0530

    Fix ExpressError constructor argument order

--- utils/ExpressError.js
+++ utils/ExpressError.js
@@ -1,10 +1,20 @@
 
+// class ExpressError extends Error {
+//     constructor(statusCode,message) {
+//         super();
+//         this.statusCode= statusCode;
+//         this.message = message;
+//     }
+// }
+
+// module.exports  = ExpressError;
+
+
 class ExpressError extends Error {
-    constructor(statusCode,message) {
-        super();
-        this.statusCode= statusCode;
-        this.message = message;
-    }
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
 }
 
-module.exports  = ExpressError;
+module.exports = ExpressError;
